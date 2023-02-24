fabb080ac54181215f29bceb16cb7a4d0cb1e034
Michael9921 <michael.dharmawan1@student.umn.ac.id>
2023-02-24 21:02:09 +0700

    Learning why (almost) everything has a value

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
-    println("Hello,  ${args[0]}")
+    println("Hello, ${args[0]}")
+
+    // Will assign kotlin.Unit
+    val isUnit = println("This is an expression")
+    println(isUnit)
+
+    val temperature = 10
+    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK"
+    }."
+    println(message)
+
 } 
